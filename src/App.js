5fb836154957f145f1a72dce8dbc3217e1200c06
Ryan Lassiter <ryankylelassiter@gmail.com>
2020-06-16 00:01:20 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import AppRouter from './router/AppRouter';
+import ModalContainer from './components/overlays/ModalContainer';
+import SideDrawerContainer from './components/overlays/SideDrawerContainer';
+import BackDrop from './components/styled-components/UI/BackDrop';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './components/styled-components/GlobalStyles';
+import theme from './components/styled-components/UI/theme';
+import { closeModal, closeSideDrawer } from './store/actions/overlaysActions';
 import './App.css';
 
-function App() {
+const App = ({ 
+  showModal, 
+  modal,
+  showSideDrawer,
+  sideDrawer,
+  closeModal, 
+  closeSideDrawer 
+}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <AppRouter>
+        <BackDrop show={showSideDrawer} onClick={closeSideDrawer} />
+        <SideDrawerContainer sideDrawer={sideDrawer} />
+        <BackDrop show={showModal} onClick={closeModal} />
+        <ModalContainer modal={modal} />
+      </AppRouter>
+    </ThemeProvider>
   );
-}
+};
 
-export default App;
+const mapDispatch = dispatch => ({
+  closeModal: () => dispatch(closeModal()),
+  closeSideDrawer: (sideDrawerType) => dispatch(closeSideDrawer(sideDrawerType)),
+})
+
+const mapState = state => ({
+  showModal: state.overlays.modal.show,
+  modal: state.overlays.modal,
+  showSideDrawer: state.overlays.sideDrawer.show,
+  sideDrawer: state.overlays.sideDrawer
+})
+
+export default connect(mapState, mapDispatch)(App);
